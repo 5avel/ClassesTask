cdddca5cb4f6b726fb9ed5463a137d804bd9a327
5avel <5avel@hotmail.com>
2019-12-04 19:21:20 +0200

    T4 LengthSide

---
+++ Task4/Figure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4
+{
+    public class Figure
+    {
+        private readonly Point[] points;
+
+        public Figure(Point point1, Point point2, Point point3)
+        {
+            this.points = new Point[] {point1, point2, point3 };
+        }
+
+        public Figure(Point point1, Point point2, Point point3, Point point4)
+        {
+            this.points = new Point[] { point1, point2, point3, point4 };
+        }
+
+        public Figure(Point point1, Point point2, Point point3, Point point4, Point point5)
+        {
+            this.points = new Point[] { point1, point2, point3, point4, point5 };
+        }
+
+        private double LengthSide(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+        }
+    }
+}
---
+++ Task4/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4
+{
+    public class Point
+    {
+        private int x;
+        public int X { get => x; }
+
+        private int y;
+        public int Y { get => y; }
+
+        private string name;
+        public string Name { get => name; }
+
+        public Figure(int x, int y, string name)
+        {
+            this.x = x;
+            this.y = y;
+            this.name = name;
+        }
+    }
+}
